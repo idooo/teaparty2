9339503c1c74eeb361ae60cd1e6493ef3c3680f1
Alex Shteinikov <alex.shteinikov@gmail.com>
2014-12-10 20:36:17 +1000

    forgot file

---
+++ server.js
@@ -0,0 +1,18 @@
+var config = require(process.env.conf || './config/default.json'),
+    restify = require('restify'),
+    socketio = require('socket.io');
+
+var server = restify.createServer(),
+    io = socketio.listen(server.server);
+
+server.use(restify.bodyParser());
+
+var model = require('./core/database')(config);
+console.log(model);
+require('./core/routing')(server, model);
+require('./core/updater');
+require('./core/sockets')(io);
+
+server.listen(config.server.port, function () {
+    console.log('socket.io server listening at %s', server.url);
+});
