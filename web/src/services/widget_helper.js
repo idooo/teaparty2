03565ad961abe98366cfe62ab8de2fe45adbd74b
Alex Shteinikov <alex.shteinikov@gmail.com>
2015-01-16 23:26:23 +1000

    WidgetHelper service was added

---
+++ web/src/services/widget_helper.js
@@ -0,0 +1,30 @@
+'use strict';
+
+angular.module("app.services")
+    .service('WidgetHelper', function () {
+
+        /**
+         * Change font size of text element to fill the parent element
+         * Thanks to: https://github.com/jquery-textfill/jquery-textfill/
+         *
+         * @param textElement
+         * @param parentWidth
+         * @param parentHeight
+         */
+        this.textFill = function(textElement, parentWidth, parentHeight) {
+            var fontSize = 100, // Max font size
+                minFontSize = 10,
+                paddings = 10,
+                textHeight,
+                textWidth;
+
+            do {
+                textElement.style.fontSize = fontSize + 'px';
+                textHeight = textElement.offsetHeight;
+                textWidth = textElement.offsetWidth;
+                fontSize = fontSize - 1;
+            } while (
+                (textHeight + paddings > parentHeight || textWidth + paddings > parentWidth)
+                && fontSize > minFontSize);
+        };
+    });
