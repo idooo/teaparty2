4dd7f217d045e2239260cd2c3f21e04f5020ec4b
Alex Shteinikov <alex.shteinikov@gmail.com>
2015-02-07 12:16:22 +1000

    auto scale list widget

--- widgets/list/view/list.js
+++ widgets/list/view/list.js
@@ -1,7 +1,8 @@
-'use strict';
 
 angular.module('teaparty2.widgets')
     .directive('widgetList', function(TemplatePath) {
+
+    const RESIZE_SCALE_DELAY = 500;
 
     return {
         restrict: 'E',
@@ -10,8 +11,30 @@ angular.module('teaparty2.widgets')
             widget: '=widget'
         },
         templateUrl: TemplatePath.get('list'),
-        controller: function($scope, $element, $attrs, WidgetSubscriber)  {
-            WidgetSubscriber.update($scope);
+        link: function(scope, element) {
+            scope.listElement = Sizzle('.widget-list-container', element[0])[0];
+        },
+        controller: function($scope, $element, $attrs, $timeout, WidgetSubscriber)  {
+
+            $scope.autoScale = (function () {
+                return function(delay) {
+                    $timeout(function() {
+                        $scope.listElement.style.transform = 'scale(1)';
+                        if ($scope.listElement.offsetHeight > $element[0].offsetHeight) {
+                            let scaleRatio = $element[0].offsetHeight / $scope.listElement.offsetHeight;
+                            $scope.listElement.style.transform = `scale(${scaleRatio})`;
+                        }
+                    }, delay)
+                }
+            })();
+
+            WidgetSubscriber.ready($scope, $scope.autoScale);
+
+            WidgetSubscriber.update($scope, $scope.autoScale);
+
+            WidgetSubscriber.sizeChange($scope, function() {
+                $scope.autoScale(RESIZE_SCALE_DELAY)
+            });
         }
-    }
+    };
 });
